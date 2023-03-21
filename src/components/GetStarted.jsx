dc34dab4a0e45788b1eca917693b29a5b85fc041
Mateus Fernandes <mateusowlfernandes@gmail.com>
2023-03-21 19:53:45 -0300

    Tried something. Didn't work unfortunately

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -4,7 +4,7 @@ import { arrowUp } from '../assets';
 const GetStarted = () => {
   return (
     <div
-      className={`group ${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+      className={`${styles.flexCenter} group w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
     >
       <div
         className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
